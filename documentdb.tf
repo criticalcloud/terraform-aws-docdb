ffbd60af3fd7abe0d253bcc4d5e201f2d2c5184c
paulo.silva@captalys.com.br <paulo.silva@captalys.com.br>
2021-08-16 11:31:05 -0300

    create module

---
+++ documentdb.tf
@@ -0,0 +1,61 @@
+## DOCUMENTDB
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = var.cluster_identifier
+  engine                  = "docdb"
+  engine_version          = var.engine_version
+  master_username         = var.username
+  master_password         = var.password
+  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.docdb_pg.name
+  db_subnet_group_name = var.db_subnet_group_name
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  backup_retention_period = var.backup_retention_period
+  preferred_backup_window = var.preferred_backup_window
+  preferred_maintenance_window = var.preferred_maintenance_window
+  
+  storage_encrypted = var.storage_encrypted
+  deletion_protection = var.deletion_protection
+  skip_final_snapshot     = var.skip_final_snapshot
+
+  tags = merge(
+    var.tags,
+    {
+      "Name" = var.cluster_identifier
+    },
+  )
+
+}
+
+## MONGODB INSTANCES
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  identifier         = local.repl_name
+  cluster_identifier = aws_docdb_cluster.docdb.id
+  instance_class     = var.instance_class
+  preferred_maintenance_window = var.preferred_maintenance_window
+  promotion_tier = var.promotion_tier
+
+  tags = merge(
+    var.tags,
+    {
+      "Name" = local.repl_name
+    },
+  )
+  
+  count              = var.cluster_instances_count
+}
+
+## PARAMETER GROUP
+resource "aws_docdb_cluster_parameter_group" "docdb_pg" {
+  family      = var.pg_family
+  name        = local.pg_name
+  description = var.pg_description
+
+
+  dynamic "parameter" {
+    for_each = var.parameters
+    content {
+      name         = parameter.value.name
+      value        = parameter.value.value
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,114 @@
+# Mandatory
+variable "password" {
+  type = string
+}
+
+variable "db_subnet_group_name" {
+    type = string
+}
+
+# Variables with default values
+variable "tags" {
+  description = "A mapping of tags to assign to the resource"
+  type        = map(string)
+  default     = {
+    "Realm" = "asgard"
+  }
+}
+
+variable "vpc_security_group_ids" {
+  type = list(string)
+  default = []
+  description = "Security group IDs"
+}
+
+variable "cluster_identifier" {
+  type = string
+  default = "mongo"
+}
+
+variable "replica_identifier" {
+    type = string
+    default = "mongo_rpl"
+}
+
+variable "engine_version" {
+    type = string
+    default = "4.0.0"
+}
+
+variable "username" {
+  type = string
+  default = "admin"
+}
+
+variable "backup_retention_period" {
+    type = number
+    default = 7
+}
+
+variable "preferred_backup_window" {
+    type = string
+    default = "03:00-03:30"
+}
+
+variable "preferred_maintenance_window" {
+    type = string
+    default = "sat:04:00-sat:04:30"
+}
+
+variable "storage_encrypted" {
+    type = bool
+    default = true
+}
+
+variable "deletion_protection" {
+    type = bool
+    default = true
+}
+
+variable "skip_final_snapshot" {
+    type = bool
+    default = false
+}
+
+
+variable "db_vpc_id" {
+  type = string
+}
+
+variable "parameter_group_name" {
+  type = string
+  default = "pg"
+}
+
+variable "cluster_instances_count" {
+    type = number
+    default = 1
+}
+
+variable "instance_class" {
+    type = string
+    default = "db.t3.medium"
+}
+
+variable "pg_family" {
+    type = string
+    default = "docdb4.0"
+}
+
+variable "pg_description" {
+    type = string
+    default = "Managed by Terraform"
+}
+
+variable "parameters" {
+  description = "A list of DB parameter maps to apply"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "promotion_tier" {
+    type = number
+    default = 1
+}
