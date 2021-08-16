af7cf67cc7a356b1b9ce20ff772a2617a5dfd0c5
paulo.silva@captalys.com.br <paulo.silva@captalys.com.br>
2021-08-16 12:03:42 -0300

    fix vars

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  pg_name = var.parameter_group_name != "pg" ? var.parameter_group_name : "${var.parameter_group_name}-${var.cluster_name}"
+  pg_name = var.parameter_group_name != "pg" ? var.parameter_group_name : "${var.parameter_group_name}-${var.cluster_identifier}"
   repl_name = var.replica_identifier != "mongo_rpl" ? var.replica_identifier : "${var.replica_identifier}-${sum([count.index, 1])}"
 }
